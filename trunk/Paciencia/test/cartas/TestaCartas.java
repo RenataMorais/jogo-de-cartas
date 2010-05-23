e5e23c0d8e89b96ea63273b01fd7a9790f0d270d
jhonny.bittencourt <jhonny.bittencourt@0adc7399-4ff4-9c71-5321-bc0df7348069>
2010-05-23 02:39:10 +0000

    Teste de geração de cartas; Teste do método que vira uma carta;

---
+++ trunk/Paciencia/test/cartas/TestaCartas.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cartas;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import paciencia.Carta;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author joaocarlos
+ */
+public class TestaCartas {
+
+    public TestaCartas() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testaViraCarta() {
+        Carta x = new Carta();
+
+        x.setStatus(true);
+        x.viraCarta(x);
+        assertFalse(x.isStatus());
+    }
+
+    @Test
+    public void testaGeraCarta() {
+        int i = 0;
+        boolean test;
+
+        test = geraCartaAux();
+        assertEquals(true, test);
+
+        test = geraCartaAux();
+        assertEquals(true, test);
+
+        test = geraCartaAux();
+        assertEquals(true, test);
+
+    }
+
+    public boolean geraCartaAux() {
+        Carta carta = new Carta();
+        boolean a = false;
+
+        carta =
+                carta.geraCarta();
+        if (carta.getValor() >= 1 && carta.getValor() <= 13) {
+            a = true;
+        } else {
+            a = false;
+        }
+
+        return a;
+    }
+}
