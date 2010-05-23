e30ef0b419fb9cf04fd83da5752715e9c70ab489
jhonny.bittencourt <jhonny.bittencourt@0adc7399-4ff4-9c71-5321-bc0df7348069>
2010-05-23 02:48:32 +0000

    Classe Carta; +Testes implementados +Método que vira as cartas; +Método que produz uma carta randômica; Classe Main +Estou testando a classe Baralho aqui.

--- trunk/Paciencia/src/paciencia/Main.java
+++ trunk/Paciencia/src/paciencia/Main.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package paciencia;
+
+import java.util.Stack;
 
 /**
  *
@@ -15,7 +16,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    }
+        Baralho b1 = new Baralho();
+        Stack<Carta> pilha = new Stack<Carta>();
+        Carta c1 = new Carta();
+        int i = 0;
 
+        /*rdm = random.geraRandom(14, 13);
+        while(i <= 12) {
+        System.out.println(rdm[i]);
+        i++;
+        }*/
+        //c1 = c1.geraCarta();
+        //System.out.println(c1.getValor());
+        pilha = b1.produzBaralho();
+        while (i <= pilha.size()-1) {
+            c1 = pilha.pop();
+            System.out.println(c1.getNaipe()+ ", " +c1.getValor());
+        }
+    }
 }
